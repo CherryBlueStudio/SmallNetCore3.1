f76576ed897da4c2001c00c36da19f4f92755eae
Steve <steve.lambert@live.co.uk>
2021-04-12 15:57:30 +0100

    Set-up files

---
+++ StevesProductShop/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StevesProductShop.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        // Slug is used to create a better looking URL
+        // by replacing spaces with dashes
+        public string Slug => Name.Replace(' ', '-');
+    }
+}
