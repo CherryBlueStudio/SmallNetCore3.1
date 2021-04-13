1b0661d5e65fb271dee7db7392d0464e27d66e42
Steve <steve.lambert@live.co.uk>
2021-04-13 19:59:27 +0100

    Add product database example data

--- StevesProductShop/Models/Product.cs
+++ StevesProductShop/Models/Product.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace StevesProductShop.Models
+﻿namespace StevesProductShop.Models
 {
     public class Product
     {
