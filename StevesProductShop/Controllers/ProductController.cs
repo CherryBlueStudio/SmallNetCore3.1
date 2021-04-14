75f857df258ec894216219315de185cae43f56b6
Steve <steve.lambert@live.co.uk>
2021-04-14 11:35:07 +0100

    Tidy up base files

---
+++ StevesProductShop/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StevesProductShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StevesProductShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Detail(string slugLink)
+        {
+            Product product = Database.GetProduct(slugLink);
+
+            return View(product);
+        }
+
+        public IActionResult List()
+        {
+            List<Product> products = Database.GetProducts();
+
+            return View(products);
+        }
+    }
+}
